d28fa6dd50fd0d6db6a289cad91f8d670b5e6a9f
jesakim <jesakim1@gmail.com>
2024-01-04 10:48:42 +0100

    adding zentities

--- src/main/java/com/example/taskflow/TaskFlowApplication.java
+++ src/main/java/com/example/taskflow/TaskFlowApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskFlowApplication {
+public class TaskFlowApplicati {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskFlowApplication.class, args);
